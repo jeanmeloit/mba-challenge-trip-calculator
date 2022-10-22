673d30a9e958e120c84a65900de617e9ed8e9687
Jean Melo <jean.melo.it@outlook.com>
2022-10-22 16:26:03 -0300

    (feat): Toolbar relocation

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -3,7 +3,14 @@ import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'jsh-features',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+    <p-menubar styleClass="mb-2">
+      <ng-template pTemplate="start">
+        <img src="assets/travel/logo.png" height="40" class="mr-2" />
+      </ng-template>
+    </p-menubar>
+    <router-outlet></router-outlet>
+  `
 })
 export class FeaturesComponent implements OnInit {
   constructor(private primengConfig: PrimeNGConfig) {}
