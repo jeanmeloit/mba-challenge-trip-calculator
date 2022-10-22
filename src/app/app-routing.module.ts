2994a17c5961fe03f63b0f5f52c498371b6ff3b5
Jean Melo <jean.melo.it@outlook.com>
2022-10-22 16:52:43 -0300

    (feat): enable hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
+import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./features/features.module').then((m) => m.FeaturesModule),
+    loadChildren: () => import('./features/features.module').then((m) => m.FeaturesModule)
   },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes, { useHash: environment.production })],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
